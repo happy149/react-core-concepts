2bd0c72fd12992b50c2ba05c9b93c02424d1cb47
happy149 <hosnearahappybd@gmail.com>
2021-02-23 19:56:12 +0600

    style

--- core-concepts/src/App.js
+++ core-concepts/src/App.js
@@ -2,33 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var person = {
-    name:"Dr.Mahfuz",
-    job:"Singer"
-  }
-  var person2 = {
-    name:"Eva Rahman",
-    job:"kokil kondhi"
-  }
-  var style = {
-    color:'red',
-    backgroundColor:'yellow'
-  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <p>My first react paragraph</p>
-        <h1>My heading : {(2+5)*5*6}</h1>
-        <h1 style={style}>Singer: {person.name+" " +person.job}</h1>
-        <h3 style= {{backgroundColor:'cyan', color:'yellow'}}>My heading : {person2.name+" " +person2.job}</h3>
-      
+        <p>I am a React Person</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        
+        
+        
       </header>
     </div>
   );
+}
+function Person(){
+  const PersonStyle={
+    border: '2px solid red', 
+    margin:'10px'
+
+  }
+  return (
+  <div style={PersonStyle}>
+    <h1> Name:Hosneara Happy</h1> 
+    <h3> i am a mother of Ayesha Alia</h3>
+    </div>
+  )
 }
 
 export default App;
