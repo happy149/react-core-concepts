19e104b47eaab1ed6522852fd762bbf5c188de28
happy149 <hosnearahappybd@gmail.com>
2021-02-23 18:18:36 +0600

    js with Html

--- core-concepts/src/App.js
+++ core-concepts/src/App.js
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name:"Dr.Mahfuz",
+    job:"Singer"
+  }
+  var person2 = {
+    name:"Eva Rahman",
+    job:"kokil kondhi"
+  }
+  var style = {
+    color:'red',
+    backgroundColor:'yellow'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +21,10 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
+        <p>My first react paragraph</p>
+        <h1>My heading : {(2+5)*5*6}</h1>
+        <h1 style={style}>Singer: {person.name+" " +person.job}</h1>
+        <h3 style= {{backgroundColor:'cyan', color:'yellow'}}>My heading : {person2.name+" " +person2.job}</h3>
       
       </header>
     </div>
