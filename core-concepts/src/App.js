c003ad7aaf8542eb39c48c02999c0650cebdfee5
happy149 <hosnearahappybd@gmail.com>
2021-02-24 12:23:03 +0600

    32-8

--- core-concepts/src/App.js
+++ core-concepts/src/App.js
@@ -3,17 +3,41 @@ import './App.css';
 
 function App() {
   const nayoks = ['Anwar', 'Jafor', 'Alomgir', 'Salman']
+  const products = [
+    {name: 'Photoshop', price: '$99.99'},
+    {name: 'Illustrator', price: '$60.99'},
+    {name: 'PDF Reader', price: '$6.99'}
+  ]
   return (
     <div className="App">
       <header className="App-header">
         <p>I am a React Person</p>
-        <Person name="Munna" job="football"></Person>
-        <Person name="Abul" job="Dorshok"></Person>
+        <Product product={products[0]}></Product>
         
       </header>
     </div>
   );
 }
+
+function Product(props){
+  
+    const productStyle={
+      border: '1px solid grey',
+      borderRadius: '5px',
+      backgroundColor: 'lightgrey',
+      height: '200px',
+      width: '200px',
+      float: 'left'
+    }
+    return (
+      <div style={productStyle}>
+        <h2>{props.product.name}</h2>
+        <h5>{props.price}</h5>
+        <button>Buy now</button>
+      </div>
+  )
+}
+
 
 function Person(props){
    return (
