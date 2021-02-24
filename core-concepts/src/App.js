992dd203be7a2f69ea8a8d4e28c793aca2f1d5f4
happy149 <hosnearahappybd@gmail.com>
2021-02-24 17:41:15 +0600

    count32-11

--- core-concepts/src/App.js
+++ core-concepts/src/App.js
@@ -49,9 +49,17 @@ function App() {
 }
 function Counter(){
   const [count, setCount] = useState(10);
+  const handleIncrease = () => setCount(count + 1);
+    //const newCount = count + 1;
+    //setCount(newCount);
+    //setCount(count + 1);
+  
+
   return(
   <div>
     <h1>Count: {count}</h1>
+    <button onClick={() => setCount(count - 1)}>Decrease</button>
+    <button onClick={() =>setCount(count + 1)}>Increase</button>
     </div>)
   
 }
